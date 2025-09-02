e478cf36137529a381c4fbd40b4c215df6b3ecda
AarushKashyap92 <aarush.2428010098@muj.manipal.edu>
2025-09-02 22:52:24 +0530

    Add files via upload

---
+++ 17_10to1_for_loop.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i = 10;   // initialization
+
+    do {
+        cout << i << " ";
+        i--;      // decrement
+    } while (i > 0);  // condition
+
+    return 0;
+}
---
+++ 1_multiline.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+void main ()
+{
+    cout << "C++ is a Object oreanted programming.";
+    cout << "C is Processor Oreanted Programming.";
+}
